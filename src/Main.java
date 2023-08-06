f7a7a5ef18eae09a7164cda3c1ffccd9c85c1115
Aliya <seitkassymovaa@gmail.com>
2023-08-06 14:15:56 +0300

    MobileOperatorBonus

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int initialScore = 250; //Исходный счет
-        int replenishmentAmount = 1700; //Сумма пополнения
+        int initialScore = 100; //Исходный счет
+        int replenishmentAmount = 1100; //Сумма пополнения
         int bonusAmount = replenishmentAmount / 100; //Количество бонусов
-        int finalScore = replenishmentAmount + initialScore + bonusAmount; //Итоговый счет
-        if (replenishmentAmount > 1000) {
+        int finalScore = replenishmentAmount + initialScore + bonusAmount;
+
+        if (replenishmentAmount >= 1000) { bonusAmount = replenishmentAmount / 100;
             System.out.println("Бонусы =" + bonusAmount);
+        } else {
+            bonusAmount = 0;
+            finalScore = replenishmentAmount + initialScore;
         }
         System.out.println("Исходный счет =" + initialScore);
         System.out.println("Сумма пополнения =" + replenishmentAmount);
